b467ec9284f6ab2c8784c2d91e5ae7a6b65750e2
Daniel Shaefer <daniel@dwolla.com>
2016-05-10 17:23:49 -0500

    Update scripts to have a build step that runs linter and tests. Fixed jshint errors

--- src/RandomChart.js
+++ src/RandomChart.js
@@ -3,25 +3,25 @@ function RandomChart(myItems) {
 
 	this.get = function(times) {
 		if (!times)
-			times = 1
+			times = 1;
 		var selectedItems = [];
 		for(var i = 0;i<times;i++) {
-			var index = Math.floor(Math.random() * items.length)
-			var item = items[index]
-			selectedItems.push({index:index, text:item})
+			var index = Math.floor(Math.random() * items.length);
+			var item = items[index];
+			selectedItems.push({index:index, text:item});
 		}
 		
 		return selectedItems;
-	}
+	};
 
 	this.getAll = function() {
 		return items;
-	}
+	};
 }
 
 RandomChart.create = function(className, itemList) {
 	window[className] = function() {
 		RandomChart.call(this, itemList);
-	}
+	};
 	window[className].prototype = Object.create(RandomChart.prototype);
-}
+};
