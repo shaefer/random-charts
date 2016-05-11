23c4d27ad41ca280d77987e058db2726c3ec7777
Daniel Shaefer <daniel@dwolla.com>
2016-05-11 13:59:43 -0500

    Updated to use seedrandom library for seeding and more uniform random number generation. Also, added graceful fallback if dependency is missing

--- src/RandomChart.js
+++ src/RandomChart.js
@@ -1,12 +1,22 @@
-function RandomChart(myItems) {
+if (!Math.seedrandom) console.warn("Math.seedrandom function is missing. Seedrandom library (https://github.com/davidbau/seedrandom) most likely has not been loaded. This will gracefully fallback to Math.random() behavior.")
+
+function RandomChart(myItems, randomSeed) {
 	var items = myItems;
+	var random = Math.random;
+	if (Math.seedrandom) {
+    if (randomSeed) {
+      random = new Math.seedrandom(randomSeed);
+    } else {
+      Math.seedrandom();
+    }
+	}
 
 	this.get = function(times) {
 		if (!times)
 			times = 1;
 		var selectedItems = [];
 		for(var i = 0;i<times;i++) {
-			var index = Math.floor(Math.random() * items.length);
+			var index = Math.floor(random() * items.length);
 			var item = items[index];
 			selectedItems.push({index:index, text:item});
 		}
@@ -20,8 +30,8 @@ function RandomChart(myItems) {
 }
 
 RandomChart.create = function(className, itemList) {
-	window[className] = function() {
-		RandomChart.call(this, itemList);
+	window[className] = function(randomSeed) {
+		RandomChart.call(this, itemList, randomSeed);
 	};
 	window[className].prototype = Object.create(RandomChart.prototype);
 };
