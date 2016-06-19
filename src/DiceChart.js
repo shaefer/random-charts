a48f7b7ede6d5b28bed87a9bf398b3bf1f2e336d
Daniel Shaefer <shaefer@gmail.com>
2016-06-18 22:49:38 -0500

    Clean up lint errors

--- src/DiceChart.js
+++ src/DiceChart.js
@@ -1,7 +1,7 @@
 var DiceChart = Object.create(RandomChart);
 DiceChart.add = function(a, b) {
     return a + b;
-}
+};
 
 DiceChart.rollDice = function(numOfDice, numOfSides, random) {
   var diceRolled = [];
@@ -13,12 +13,12 @@ DiceChart.rollDice = function(numOfDice, numOfSides, random) {
   var sum = diceRolled.reduce(DiceChart.add, 0);
   var result = {total: sum, diceRolled: diceRolled};
   return result;
-}
+};
 
 DiceChart.create = function(numOfDice, numOfSides, itemList, randomSeed) {
   var numOfItemsPossibleWithDice = (numOfDice * numOfSides) - numOfDice + 1;
   if (numOfItemsPossibleWithDice != itemList.length)
-    throw new Error("A chart of "+numOfDice+"d"+numOfSides+" items should have "+numOfItemsPossibleWithDice+" items. You have specified "+itemList.length+".")
+    throw new Error("A chart of "+numOfDice+"d"+numOfSides+" items should have "+numOfItemsPossibleWithDice+" items. You have specified "+itemList.length+".");
 
     var diceChart = RandomChart.create("myDiceChart", itemList, randomSeed);
 
