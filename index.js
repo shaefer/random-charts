1b648160cb5438439eb12b3dd6f84dbe60895846
Daniel Shaefer <shaefer@gmail.com>
2016-06-24 23:43:59 -0500

    Exporting core classes for npm

---
+++ index.js
@@ -0,0 +1,11 @@
+var RandomChart = require('./lib/RandomChart'),
+    LinkedChart = require('./lib/LinkedChart'),
+    DiceRollItemSelection = require('./lib/selectionMethodologies/DiceRollItemSelection'),
+    SimpleRandomItemSelection = require('./lib/selectionMethodologies/SimpleRandomItemSelection');
+
+module.exports = {
+    RandomChart: RandomChart,
+    LinkedChart: LinkedChart,
+    DiceRollItemSelection: DiceRollItemSelection,
+    SimpleRandomItemSelection: SimpleRandomItemSelection
+};
