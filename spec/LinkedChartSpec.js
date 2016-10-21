8e9583f3b2ccdfb6f3ddcc8d3cc98b98ddfc9428
Daniel Shaefer <daniel@dwolla.com>
2016-10-21 13:41:03 -0500

    Hooked in subtable linking and deep getting

--- spec/LinkedChartSpec.js
+++ spec/LinkedChartSpec.js
@@ -1,13 +1,27 @@
 var LinkedChart = require('../src/LinkedChart'),
     RandomChartSpec = require('./RandomChartSpecHelper');
+var expect = require('chai').expect;
 
-describe('ChainableChart', function() {
+describe('LinkedChart', function() {
   describe('create', function () {
     it('should create a new class that can be instantiated and results returned', function() {
-      var chart = new LinkedChart("myChart", ["One1", "Two2", "Three3"]);
-      var amount = 3;
-      RandomChartSpec.verifyGet(chart, amount);
-      RandomChartSpec.verifyMultipleGet(chart, amount);
+
+      const subTable = {
+          linked: false,
+          name: "A SubTable",
+          items: ["Chart2-A", "Chart2-B", "Chart2-C"]
+      };
+      const mainTable = {
+        name: "Some Linked Chart",
+        items: ["Chart1-1", "Chart1-2", "Chart1-3", {linkedTable:subTable.name, description:"Roll on Subtable: " + subTable.name}]
+      };
+
+
+
+      var chart = new LinkedChart(mainTable.name, mainTable.items, [subTable], 3);
+      var result = RandomChartSpec.verifyGet(chart);
+        expect(result.index).to.be.eql(3);
+      console.log(result);
     });
   });
 });
--- src/LinkedChart.js
+++ src/LinkedChart.js
@@ -1,8 +1,42 @@
-var RandomChart = require('./RandomChart');
+var SimpleRandomItemSelection = require('./selectionMethodologies/SimpleRandomItemSelection'),
+    seedrandom = require('seedrandom'),
+    RandomChart = require('./RandomChart');
 
-module.exports = class LinkedChart extends RandomChart {
-  constructor(chartName, itemList, linkedChart, randomSeed) {
-    super(chartName, itemList, randomSeed);
-    this.linkedChart = linkedChart;
-  }
+module.exports = class LinkedChart {
+    constructor(chartName, items, subTables, randomSeed, itemSelectionMethod = new SimpleRandomItemSelection()) {
+        this.chartName = chartName;
+        this.items = items;
+        var linkedCharts = {};
+        subTables.forEach(function(table) {
+          if (table.linked)
+            linkedCharts[table.name] = new LinkedChart(table.name, table.items, table.subTables, randomSeed, itemSelectionMethod);
+          else
+            linkedCharts[table.name] = new RandomChart(table.name, table.items, randomSeed, itemSelectionMethod);
+        });
+        linkedCharts[chartName] = this; //add reference to self so that tables can tell you to roll twice on this table.
+        this.linkedCharts = linkedCharts;
+
+        if (randomSeed) {
+            this.random = seedrandom(randomSeed);
+            this.randomSeed = randomSeed;
+        } else {
+            this.random = seedrandom();
+        }
+
+        this.itemSelectionMethod = itemSelectionMethod;
+    }
+
+    get(times = 1) {
+        const items = this.items;
+        let selectedItems = [];
+        for(let i = 0;i<times;i++) {
+            let item = this.itemSelectionMethod.getItem(items, this.random);
+            if (item.item.linkedTable) {
+                //item.linkDescription will have the text `Roll on Table "Magic Items"`
+                item.result = this.linkedCharts[item.item.linkedTable].get(item.item.times || 1);
+            }
+            selectedItems.push(item);
+        }
+        return selectedItems;
+    }
 };
