ffe34bf33aeefc13adc73b1acf03776240099fc6
Daniel Shaefer <daniel@dwolla.com>
2017-05-05 15:02:41 -0500

    Updated setup for publishing to better handle distributed files

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var RandomChart = require('./lib/RandomChart'),
-    LinkedChart = require('./lib/RandomChart'),
-    DiceRollItemSelection = require('./lib/selectionMethodologies/DiceRollItemSelection'),
-    SimpleRandomItemSelection = require('./lib/selectionMethodologies/SimpleRandomItemSelection');
+var RandomChart = require('./dist/RandomChart'),
+    LinkedChart = require('./dist/RandomChart'),
+    DiceRollItemSelection = require('./dist/selectionMethodologies/DiceRollItemSelection'),
+    SimpleRandomItemSelection = require('./dist/selectionMethodologies/SimpleRandomItemSelection'),
+    GetRandomGenerator = require('./dist/models/GetRandomGenerator'),
+    TreasureTypeB = require('./dist/data/charts/treasureByType/TreasureTypeB');
 
 module.exports = {
     RandomChart: RandomChart,
     LinkedChart: LinkedChart,
     DiceRollItemSelection: DiceRollItemSelection,
-    SimpleRandomItemSelection: SimpleRandomItemSelection
+    SimpleRandomItemSelection: SimpleRandomItemSelection,
+    GetRandomGenerator: GetRandomGenerator,
+    TreasureTypeB: TreasureTypeB
 };
